b292e121d43b94ddf720f74ff64f13d94c7a0308
aktnb <70847394+aktnb@users.noreply.github.com>
2023-06-10 03:58:32 +0900

    さんばか分類器v1 追加

---
+++ bot/src/commands/sanbaka.ts
@@ -0,0 +1,38 @@
+import { CommandInteractionOptionResolver, SlashCommandBuilder } from "discord.js";
+import { CommandHandler } from "../core";
+import { SanbakaController } from "../controller/SanbakaController";
+
+export const handler = new CommandHandler(
+  new SlashCommandBuilder()
+    .setName('sanbaka')
+    .setDescription('画像がさんばかの誰かを推測するよ')
+    .addAttachmentOption(option =>
+      option
+        .setName('face')
+        .setDescription('分類したい画像')
+        .setRequired(true)),
+  async interaction => {
+    if (interaction.user.bot) {
+      return;
+    }
+
+    const options = interaction.options as CommandInteractionOptionResolver;
+    const image = options.getAttachment('image')
+    if (!image || (image && !image.url)) {
+      await interaction.reply('画像が無いかも？');
+      return;
+    }
+
+    await interaction.deferReply();
+
+    try {
+      const predict = await SanbakaController.predict(image.url);
+      await interaction.editReply({ content: `この画像は \`${predict}\` じゃないかな～`, files: [image.url] });
+    } catch (e) {
+      if (e instanceof Error)
+        await interaction.editReply({ content: e.message, files: [image.url] });
+      else
+        await interaction.editReply('予測中に予期せぬエラーが発生しました');
+    }
+  }
+);
